374c72c5221f4ad199354ae315950824fbcae007
M-hell <sarkarsamrat16112004@gmail.com>
2025-08-14 00:26:42 +0530

    env configurations

--- activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
+++ activityservice/src/main/java/com/fitness/activityservice/ActivityserviceApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 //for date and time issue 
 @Configuration
 @EnableMongoAuditing
@@ -12,6 +14,13 @@ import org.springframework.data.mongodb.config.EnableMongoAuditing;
 public class ActivityserviceApplication {
 
 	public static void main(String[] args) {
+				// Load .env file
+        Dotenv dotenv = Dotenv.load();
+
+        // Make vars visible to Spring Boot
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
 		SpringApplication.run(ActivityserviceApplication.class, args);
 	}
 
--- aiservice/src/main/java/com/fitness/aiservice/AiserviceApplication.java
+++ aiservice/src/main/java/com/fitness/aiservice/AiserviceApplication.java
@@ -5,12 +5,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @Configuration
 @EnableMongoAuditing
 @SpringBootApplication
 public class AiserviceApplication {
 
 	public static void main(String[] args) {
+		// Load .env file
+        Dotenv dotenv = Dotenv.load();
+
+        // Make vars visible to Spring Boot
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
 		SpringApplication.run(AiserviceApplication.class, args);
 	}
 
